a73c64ac64068753bda577fdd6c885ae605a1a93
Franco317 <147573986+Franco317@users.noreply.github.com>
2023-11-11 21:13:43 -0300

    Add files via upload

--- francopy.py
+++ francopy.py
@@ -2,13 +2,17 @@ import random
 print ("que dificultad quiere? 1_facil o 2_dificil")
 x3= (int(input()))
 if (x3==1):
+    print("el numero se encuentra entre el 1-20")
+    print("tenes 4 intentos nomas")
     secreto=random.randint(1,20)
-    intm=3
+    intm=4
 intr=0    
 x=0
 if (x3==2):
+    print("el numero se encuentra entre el 1-200")
+    print("tenes 10 intentos nomas")
     secreto=random.randint(1,200)
-    intm=7
+    intm=10
 while (intr < intm):
     intr=intr+1
     x=int(input("ingrese un numero"))
@@ -19,3 +23,5 @@ while (intr < intm):
       print("el numero secreto es mayor al numero ingresado")
     else:
         print("el numero secreto es menor al numero ingresado")
+
+print("se te acabaron los intentos :(")
